20446b3d8ff7612918490fbfc65512646b8ee8b3
David Higgins <dhiggins@aimmedia.com>
2017-11-27 10:13:36 -0500

    fixed `$request['headers']['host']` to use 'Host' per Elasticsearch 5.3.1 update

--- src/ElasticSearchClientFactory.php
+++ src/ElasticSearchClientFactory.php
@@ -80,13 +80,13 @@ final class ElasticSearchClientFactory
             $secret
         ) {
             // Amazon ES listens on standard ports (443 for HTTPS, 80 for HTTP).
-            $request['headers']['host'][0] = parse_url($request['headers']['host'][0], PHP_URL_HOST);
+            $request['headers']['Host'][0] = parse_url($request['headers']['Host'][0], PHP_URL_HOST);
 
             $credentials = new \Aws\Credentials\Credentials($key, $secret);
 
             // Create a PSR-7 request from the array passed to the handler
             $psr7Request = new \GuzzleHttp\Psr7\Request($request['http_method'],
-                (new \GuzzleHttp\Psr7\Uri($request['uri']))->withScheme($request['scheme'])->withHost($request['headers']['host'][0]),
+                (new \GuzzleHttp\Psr7\Uri($request['uri']))->withScheme($request['scheme'])->withHost($request['headers']['Host'][0]),
                 $request['headers'], $request['body']);
 
             // Sign the PSR-7 request with credentials from the environment
